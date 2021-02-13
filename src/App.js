560ae455771d0fbcfbbe34f1076fbaba200f660b
aymanxdev <ayman.bacc@gmail.com>
2021-02-13 16:29:25 +0000

    add tests for all components and added jest to package.json

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import { Header } from "./components/layout/Header";
-import { Sidebar } from "./components/layout/Sidebar";
+import { Content } from "./components/layout/Content";
 import { ProjectsProvider, SelectedProjectProvider } from "./context";
-import "./App.scss";
 
-export const App = () => (
-  <SelectedProjectProvider>
-    <ProjectsProvider>
-      <div>
-        <header>
-          <Header />
-          <Sidebar />
-        </header>
-      </div>
-    </ProjectsProvider>
-  </SelectedProjectProvider>
-);
+export const App = ({ darkModeDefault = false }) => {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
+
+  return (
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <main
+          data-testid="application"
+          className={darkMode ? "darkmode" : undefined}
+        >
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Content />
+        </main>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
+  );
+};
+
+App.propTypes = {
+  darkModeDefault: PropTypes.bool,
+};
